aa836d8a50ab16fb7c56a3297a0f5f5244c091d3
Himanshu Soni <himanshua790@gmail.com>
2024-03-06 14:31:39 +0530

    chore: updated resume

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -13,7 +13,7 @@ const Resume = (props: Props) => {
           height: "100%",
         }}
         type="application/pdf"
-        src={"/Himanshu_Soni_Fullstack_v1.2.pdf"}
+        src={"/Himanshu_Soni_Fullstack_v1.3.pdf"}
         about="Himanshu's Fullstack Resume"
         title="Himanshu's Fullstack Resume"
       />
